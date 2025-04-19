0c7f2e388c55b08533f97b834b4c02c58e96f861
Snack <nurd_galim@mail.ru>
2025-04-20 01:39:10 +0700

    some refactor for cleaning

--- dataset/main.py
+++ dataset/main.py
@@ -1,14 +1,13 @@
 import os
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 from html2text import HTML2Text
 import re
 
-
-def main():
+def load_db_config():
     load_dotenv()
-    db_config = {
+    return {
         "host": os.getenv("DB_HOST"),
         "port": os.getenv("DB_PORT"),
         "user": os.getenv("DB_USER"),
@@ -17,26 +16,58 @@ def main():
         "dialect": os.getenv("DB_DIALECT"),
     }
 
+def get_engine():
+    db_config = load_db_config()
+
     connection_string = (
         f"{db_config['dialect']}://{db_config['user']}:{db_config['password']}"
         f"@{db_config['host']}:{db_config['port']}/{db_config['database']}"
     )
-    engine = create_engine(connection_string)
+    return create_engine(connection_string)
+
+def define_sql_function(engine):
+    sql_function = """
+CREATE FUNCTION IF NOT EXISTS clean_text(input_text LONGTEXT)
+    RETURNS LONGTEXT
+    DETERMINISTIC
+    BEGIN
+        DECLARE cleaned LONGTEXT;
+        SET cleaned = REGEXP_REPLACE(input_text, '<[^>]+>', ' ');
+        SET cleaned = REGEXP_REPLACE(cleaned, '<br />', ' ');
+        SET cleaned = REGEXP_REPLACE(cleaned, '[\r\n]+', ' ');
+        SET cleaned = REGEXP_REPLACE(cleaned, '[ ]+', ' ');
+        SET cleaned = REGEXP_REPLACE(cleaned, ' \\\\.', '\\\\.');
+        SET cleaned = TRIM(cleaned);
+
+        RETURN cleaned;
+    END
+"""
+    with engine.connect() as conn:
+        conn.execute(text(sql_function))
+        conn.commit()
+
+
+def main():
+    engine = get_engine()
+    define_sql_function(engine)
 
     query = """
 SELECT DISTINCT
-    hesk_tickets.message AS ticket_message,
+    hesk_tickets.id,
+    clean_text(hesk_tickets.message) AS ticket_message,
     REPLACE(
-        REPLACE(
-            hesk_replies.message, '<br />', ''
-        ), hesk_users.signature, ''
+        clean_text(hesk_replies.message),
+        clean_text(hesk_users.signature),
+        '[SIGNATURE]'
     ) AS reply_message,
-    hesk_categories.name
+    hesk_categories.name AS category_title
 FROM hesk_tickets
 INNER JOIN hesk_replies ON hesk_replies.replyto = hesk_tickets.id
 INNER JOIN hesk_categories ON hesk_categories.id = hesk_tickets.category
 LEFT JOIN hesk_users ON hesk_replies.staffid = hesk_users.id OR hesk_replies.staffid = 0
-WHERE hesk_tickets.id = 6870
+WHERE hesk_tickets.replies > 2 AND hesk_tickets.id = 6620 OR hesk_tickets.id = 6870
+ORDER BY hesk_tickets.id DESC
+LIMIT 100
 """
 
     email_regex = r'(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))'
@@ -46,9 +77,12 @@ WHERE hesk_tickets.id = 6870
     print(df)
     h = HTML2Text()
     h.ignore_links = True
+    h.ignore_images = True
+    h.single_line = True
+    h.body_width = 0
     for row in df.itertuples():
         text = row.reply_message
+        text = h.handle(text).strip()
         text = re.sub(email_regex, "[EMAIL]", text)
         text = re.sub(url_regex, "[URL]", text)
-        text = h.handle(text)
-        print(" ".join(text.split()) + "\n")
+        print(row.id, repr(text))
